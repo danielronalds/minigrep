a7bd1b624ec4a0242571ff73065cca344024f0a6
danielronalds <nzdanron@gmail.com>
2022-08-16 10:20:55 +1200

    The program can now read a program passed to it from cli arguments. End of chapter 12.2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     // Collecting the arguments passed to the program
@@ -11,5 +11,10 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
 
